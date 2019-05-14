bfcf339e647f64f7aca260884481ca5f24cf52e0
Samuel Yamamoto <yamamoto.samuel@gmail.com>
2019-05-14 16:32:55 -0400

    need to clean up code

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -25,71 +25,81 @@ class App extends Component {
   //////////////////////////////////////////SORT ALPHABETICALLY
   sortByName = () => {
     let copyOfActors = [...this.state.actors];
-    let contax = (n,m) =>{
-
-      let contax=0;
-      if (n.name>m.name){
-        contax =1;
-      }else if(n.name<m.name){
-        contax =-1;
+    let contax = (n, m) => {
+      let contax = 0;
+      if (n.name > m.name) {
+        contax = 1;
+      } else if (n.name < m.name) {
+        contax = -1;
       }
       return contax;
     };
     copyOfActors.sort(contax);
     this.setState({
-      actors:copyOfActors
-    })
-  }
+      actors: copyOfActors
+    });
+  };
   ///////////////////////////////////////////SORT BY POPULARITY
   sortByPop = () => {
     let copyOfActors = [...this.state.actors];
-    let contax = (n,m) =>{
-
-      let contax=0;
-      if (n.popularity<m.popularity){
-        contax =1;
-      }else if(n.popularity>m.popularity){
-        contax =-1;
+    let contax = (n, m) => {
+      let contax = 0;
+      if (n.popularity < m.popularity) {
+        contax = 1;
+      } else if (n.popularity > m.popularity) {
+        contax = -1;
       }
       return contax;
     };
     copyOfActors.sort(contax);
     this.setState({
-      actors:copyOfActors
-    })
-  }
-
+      actors: copyOfActors
+    });
+  };
+  ////////////////////////////////////////////LOOP THROUGH ACTORS/CONTACTS IN A TABLE
   loopThroughActors = () => {
     let actorsCopy = this.state.actors.map((actor, i) => {
       return (
-        
+        <div className="tbl">
           <tr key={i}>
             <td>
               <img src={actor.pictureUrl} />
             </td>
             <td>{actor.name}</td>
             <td>{actor.popularity}</td>
+            <button
+              onClick={() => {
+                this.deleteActor(i);
+              }}
+            >
+              Delete
+            </button>
           </tr>
-        
+        </div>
       );
     });
     return actorsCopy;
   };
+  /////////////////////////////////////////////DELETE SELECTED ACTOR/CONTACT
+  deleteActor = j => {
+    let actorsCopy = [...this.state.actors];
+    actorsCopy.splice(j, 1);
+    this.setState({
+      actors: actorsCopy
+    });
+  };
 
+  /////////////////////////////////////////////RENDER
   render() {
     return (
       <div className="App">
-        <caption>
-          <h1>IronContacts</h1>
-        </caption>
+        <h1>IronContacts</h1>
+
         <button onClick={this.addADude}>Add Random</button>
         <button onClick={this.sortByName}>Sort By Name</button>
         <button onClick={this.sortByPop}>Sort By Popularity</button>
         <th>Picture Name Popularity</th>
-        <table>
-
-        {this.loopThroughActors()}
-        </table>
+        <table>{this.loopThroughActors()}</table>
       </div>
     );
   }
